b768c68e69023cb8abe93d153cc85904f59fb7cc
ts-amorim <134457245+ts-amorim@users.noreply.github.com>
2023-12-21 22:37:51 -0300

    Solução desafio/Lab DIO Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,76 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// desafio.kt
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Aluno(
+        val nome: String,
+        val sobrenome: String,
+        val email: String,
+        val senha: String
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(
+        val nome: String,
+        val cargaHorariaTotal: Int,
+        val cargaHorariaVideos: Int,
+        val cargaHorariaAtividades: Int
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    private val inscritos = mutableSetOf<Aluno>()
+
+    fun matricular(alunos: List<Aluno>) {
+        val novosInscritos = alunos.filter { !inscritos.contains(it) }
+        inscritos.addAll(novosInscritos)
+        novosInscritos.forEach {
+            println("Olá ${it.nome} ${it.sobrenome}, você foi matriculado na formação $nome. Bem-vindo(a) à DIO!")
+        }
+    }
+
+    override fun toString(): String {
+        return "Formacao(nome='$nome', nivel=$nivel, conteudos=$conteudos, inscritos=$inscritos)"
+    }
+}
+
+class Dio {
+    private val formacoes = mutableListOf<Formacao>()
+
+    fun cadastrarFormacao(formacao: Formacao) {
+        formacoes.add(formacao)
+        println("Formação ${formacao.nome} foi cadastrada na DIO. Prepare-se para uma jornada incrível!")
+    }
+
+    fun listarFormacoes() {
+        println("Formações disponíveis na DIO:")
+        formacoes.forEach {
+            println("- ${it.nome}, Nível: ${it.nivel}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val dio = Dio()
+
+    val aluno1 = Aluno("João", "Silva", "joao@example.com", "senha123")
+    val aluno2 = Aluno("Maria", "Santos", "maria@example.com", "senha456")
+    val aluno3 = Aluno("Carlos", "Souza", "carlos@example.com", "senha789")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 120, 60, 60)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin", 180, 90, 90)
+
+    val formacaoKotlinBasico = Formacao("Kotlin Básico", Nivel.BASICO, listOf(conteudo1, conteudo2))
+    val formacaoJavaIntermediario = Formacao("Java Intermediário", Nivel.INTERMEDIARIO, listOf(conteudo1))
+
+    dio.cadastrarFormacao(formacaoKotlinBasico)
+    dio.cadastrarFormacao(formacaoJavaIntermediario)
+
+    dio.listarFormacoes()
+
+    // Matrícula em uma formação
+    formacaoKotlinBasico.matricular(listOf(aluno1, aluno2))
+    formacaoKotlinBasico.matricular(listOf(aluno1, aluno3)) // Carlos será matriculado
+
+    // Imprimir informações da formação após matrícula
+    println(formacaoKotlinBasico)
 }
